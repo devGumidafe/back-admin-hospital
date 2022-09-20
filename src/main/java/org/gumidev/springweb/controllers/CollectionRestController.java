2df95ed7ce659dbee860b9605cf4a474385eae45
Gumidafe Socorro <devgumidafe@gmail.com>
2022-09-20 18:30:00 +0100

    edit CrossOrigin for production

--- src/main/java/org/gumidev/springweb/controllers/CollectionRestController.java
+++ src/main/java/org/gumidev/springweb/controllers/CollectionRestController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-hospital-dashboard.vercel.app")
 @RestController
 @RequestMapping("/api")
 public class CollectionRestController {
--- src/main/java/org/gumidev/springweb/controllers/UserRestController.java
+++ src/main/java/org/gumidev/springweb/controllers/UserRestController.java
@@ -19,7 +19,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "https://front-hospital-dashboard.vercel.app")
 @RestController
 @RequestMapping("/api")
 public class UserRestController {
