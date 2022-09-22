14b0d6a2219d00aa0d4923f37fa8a558972ac14c
Gumidafe Socorro <devgumidafe@gmail.com>
2022-09-22 13:03:32 +0100

    upload project to github

--- src/main/java/org/gumidev/springweb/controllers/CollectionRestController.java
+++ src/main/java/org/gumidev/springweb/controllers/CollectionRestController.java
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "https://front-hospital-dashboard.vercel.app")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class CollectionRestController {
--- src/main/java/org/gumidev/springweb/controllers/HospitalRestController.java
+++ src/main/java/org/gumidev/springweb/controllers/HospitalRestController.java
@@ -16,7 +16,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-@CrossOrigin(origins = "https://front-hospital-dashboard.vercel.app")
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 @RequestMapping("/api")
 public class HospitalRestController {
